7b1e55d3b0fd54b09db67ad650699bd7a8b3de79
Imdni8 <tousif.rahaman@gmail.com>
2024-05-07 11:02:21 +0530

    single selection working

--- 01Accoridan/src/components/Accordian.jsx
+++ 01Accoridan/src/components/Accordian.jsx
@@ -3,28 +3,19 @@ import data from '../data/data'
 
 function Accordian() {
     const [selected, setSelected] = useState(null)
-    const [openState, setOpenState] = useState("+")
-    function handleClick(SectionId) {
-        if (selected === null) {
-            setSelected(SectionId)
-            console.log(selected)
-            setOpenState("x")
-        } else if (selected !== null) {
-            setSelected(null)
-            console.log(selected)
-            setOpenState('+')
-        }
+    function handleClick(sectionId) {
+        selected === sectionId ? setSelected(null) : setSelected(sectionId)
     }
 
   return (
     <>
-    <h1>Accordian from component</h1>
-    <div className='accordian'>
+    <h1 className="text-3xl font-bold underline mb-2">Accordian from component</h1>
+    <div>
         {data ? 
         data.map(
-            item => <div className='question' onClick={() => handleClick(item.id)}>
-                {item.question} <span>{openState}</span>
-                <div>{selected === item.id ? item.answer : <div></div>}</div>
+            item => <div className='border-2 border-blue-400 rounded-md mb-2 mx-2 p-2' onClick={() => handleClick(item.id)}>
+                {item.question} <span>+</span>
+                <div className='mt-2 text-blue-600'>{selected === item.id ? item.answer : <div></div>}</div>
             </div>) : 
         <div>No data found!</div>}
     </div>    
