09f359a064564b1908afe25f74496b062160524d
Imdni8 <tousif.rahaman@gmail.com>
2024-05-09 02:22:20 +0530

    toggling between single and multiselect working

--- 01Accoridan/src/components/Accordian.jsx
+++ 01Accoridan/src/components/Accordian.jsx
@@ -3,17 +3,33 @@ import data from '../data/data'
 
 function Accordian() {
     const [selected, setSelected] = useState(null)
-    function handleClick(sectionId) {
+    function singleSelection(sectionId) {
         selected === sectionId ? setSelected(null) : setSelected(sectionId)
+    }
+
+    //multiselection logic
+    const [isMultiSelection, setIsMultiSelection] = useState(false)
+    function multiSelectToggle() {
+        setIsMultiSelection(prev => {
+            const newState = !prev
+            console.log("multiselection is ", newState? "enabled" : "disabled")
+            return newState
+        })
+    }
+
+    const [openAccordions, setOpenAccordions] = useState([])
+    function multiSelection(sectionId) {
+        console.log("clicked section: " + sectionId)
     }
 
   return (
     <>
     <h1 className="text-3xl font-bold underline mb-2">Accordian from component</h1>
+    <button className='p-2' onClick={multiSelectToggle}>Enable multiselection</button>
     <div>
         {data ? 
         data.map(
-            item => <div className='border-2 border-blue-400 rounded-md mb-2 mx-2 p-2' onClick={() => handleClick(item.id)}>
+            item => <div className='border-2 border-blue-400 rounded-md mb-2 mx-2 p-2' onClick={isMultiSelection ? () => multiSelection(item.id) : () => singleSelection(item.id)}>
                 {item.question} <span>+</span>
                 <div className='mt-2 text-blue-600'>{selected === item.id ? item.answer : <div></div>}</div>
             </div>) : 
